78d10bf7aba3e384c55029ccdeecb81ab009e924
Sean Deetlefs <sdeetlefs@gmail.com>
2017-07-13 15:36:42 -0600

    Add files via upload

---
+++ HelloPrinter.java
@@ -0,0 +1,10 @@
+public class HelloPrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        
+        System.out.println(static double random());
+        
+    }
+}
