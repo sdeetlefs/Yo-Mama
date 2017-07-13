f41e314979f6162eec2d00b13ad1ce87d0581e54
Sean Deetlefs <sdeetlefs@gmail.com>
2017-07-13 15:47:47 -0600

    Factorial Class Code
    Our class factorial

---
+++ factorial.java
@@ -0,0 +1,23 @@
+public class factorial
+{
+    public static void main(String[] args)
+    {
+        
+        private int factorial(int num)
+        {
+
+            if (num == 0 || num == 1)
+                return 1;
+            else
+            {
+                return factorial(num - 1)*num;    
+            }
+    
+        }
+        
+        //System.out.println("Hello, World!");
+        
+        //System.out.println(static double random());
+        
+    }
+}
