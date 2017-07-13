5ab37cf6eafddf5579a66364623441520620b872
Sean Deetlefs <sdeetlefs@gmail.com>
2017-07-13 15:47:16 -0600

    Changes Made to Line 5

--- HelloPrinter.java
+++ HelloPrinter.java
@@ -2,7 +2,7 @@ public class HelloPrinter
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"); //This line prints out the words Hello, World!!
         
         System.out.println(static double random());
         
